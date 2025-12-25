fb8fc4e4f58951534c0cb2a870534ceea99bed11
nguyen van duc <ducnv0712@gmail.com>
2025-12-25 08:26:09 -0800

    bai 1

--- bai1.cpp
+++ bai1.cpp
@@ -1,48 +1,70 @@
 #include <stdio.h>
+#define MAX_SIZE 100
+//bai 1
+//a
 
-void wrap(int *a, int *b){
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+
+
+void nhap(int arr[], int *soluong){
+	int tempSoluong;
+	printf("Nhap so luong phan tu: ");
+	scanf("%d", &tempSoluong);
+	
+	while(tempSoluong <= 0 || tempSoluong > MAX_SIZE){
+		printf("So luong phai > 0 va < %d, Nhap lai: ", MAX_SIZE);
+		scanf("%d", &soluong);
+	}
+	
+	*soluong = tempSoluong;
+	
+	int chiso = 0;
+	printf("Nhap so phan tu: ");
+	while(chiso < tempSoluong){
+		scanf("%d", &arr[chiso]);
+		chiso ++;
+	}
 }
 
 void inMang(int arr[], int n){
-	for(int i = 0; i < n; i ++){
-		printf("%d ", arr[i]);		
+	int chiso = 0;
+	while(chiso < n){
+		printf("%d ", arr[chiso]);
+		chiso ++;
 	}
-	printf("\n");
 	
 }
 
-
-void sxNoiBot(int arr[], int n){
-	for(int i = 0; i < n-1; i ++){
-		printf("Buoc %d: ", i + 1);
-		for(int j = 0; j < n - i +1; j ++){
-			if(arr[j] < arr[j + 1] ){
-				wrap(&arr[j], & arr[j +1]);
-			}
+void timSoLonNhatVaNhoNhat(int arr[], int n){
+	int soLonNhat = arr[0];
+	int soNhoNhat = arr[0];
+	int viTriLonNha = 0;
+	int viTriNhoNhat = 0;
+	int chiso = 0;
+	while(chiso < n){
+		if(soLonNhat < arr[chiso]) {
+			soLonNhat = arr[chiso];
+			viTriLonNha = chiso;
 		}
-		inMang(arr, 5);
-	} 
-}
-
-void selectionSort(int arr[],int n){
-	for(int i = 0; i < n; i ++){
-			printf("Buoc %d: ", i + 1);
-		for( int j = 0; j < n-1; j ++){
-			if(arr[i] < arr[j]){
-				wrap(&arr[i], &arr[j]);
-			}
+		if(soNhoNhat > arr[chiso]){
+			soNhoNhat = arr[chiso];
+			viTriNhoNhat = chiso;
 		}
-			inMang(arr, 5);
+		chiso ++;
 	}
+	printf("\n");
+	printf("Max: %d ", soLonNhat);
+	printf("Vi tri Max: %d", viTriLonNha);
+	printf("\n");
+	printf("Min: %d ", soNhoNhat);
+	printf("Vi tri Min: %d", viTriNhoNhat);
+
 }
 
 main(){
-	int arr[5] = {1, 2, 4, 6, 5};
-	inMang(arr, 5);
-	selectionSort(arr, 5);
-	
-
+	int arr[MAX_SIZE];
+	int soluong = 0;
+	int solancantim = 0;
+	nhap(arr, &soluong);
+	inMang(arr, soluong);
+	timSoLonNhatVaNhoNhat(arr, soluong);
 }
